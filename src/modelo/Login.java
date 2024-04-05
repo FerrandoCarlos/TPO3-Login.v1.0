9b2054712c720249d8595a1dc03f7932aa6dcb68
aguz11 <agus110196@gmail.com>
2024-04-05 19:26:30 -0300

    Update Login.java
    agrega método de verificación de datos ingresados.

--- src/modelo/Login.java
+++ src/modelo/Login.java
@@ -9,5 +9,24 @@ package modelo;
  * @author nn
  */
 public class Login {
+    private String usuario;
+    private String clave;
     
+    public Login(String usuario, String clave) {
+        this.usuario = usuario;
+        this.clave = clave;
+    }
+     
+    public boolean verificarDatos(Login loginIngresado) {
+        return this.usuario.equals(loginIngresado.getUsuario()) && this.clave.equals(loginIngresado.getClave());
+    }
+
+    
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
 }
